2c52424c352acc19270c213f45a490a4f45c7ce9
Arjan <Arjan.van.Vught@gmail.com>
2026-04-27 18:31:11 +0200

    Rename firmware-template-GD32 to firmware-template-gd32
    Rename the firmware-template-GD32 directory to firmware-template-gd32 (case-only rename) across project files.

---
+++ firmware-template-gd32/Rules.mk
@@ -0,0 +1,144 @@
+$(info "Rules.mk")
+
+PREFIX ?= arm-none-eabi-
+
+CC	 = $(PREFIX)gcc
+CPP	 = $(PREFIX)g++
+AS	 = $(CC)
+LD	 = $(PREFIX)ld
+AR	 = $(PREFIX)ar
+
+BOARD?=BOARD_GD32F470VG
+ENET_PHY?=RTL8201F
+MCU?=GD32F470VG
+
+TARGET=$(FAMILY).bin
+LIST=$(FAMILY).list
+MAP=$(FAMILY).map
+SIZE=$(FAMILY).size
+BUILD=build_gd32/
+FIRMWARE_DIR=./../firmware-template-gd32/
+
+PROJECT=$(notdir $(patsubst %/,%,$(CURDIR)))
+$(info $$PROJECT [${PROJECT}])
+
+DEFINES:=$(addprefix -D,$(DEFINES))
+
+include ../common/make/gd32/Board.mk
+include ../common/make/gd32/Mcu.mk
+include ../firmware-template/libs.mk
+include ../common/make/DmxNodeNodeType.mk
+include ../common/make/DmxNodeOutputType.mk
+include ../common/make/gd32/Includes.mk
+include ../common/make/Artnet.mk
+include ../common/make/gd32/mbedtls.mk
+include ../common/make/gd32/Validate.mk
+
+LIBS+=gd32
+
+ifeq ($(findstring NODE_SHOWFILE,$(DEFINES)),NODE_SHOWFILE)
+	LIBS+=showfile
+endif
+
+LIBS+=clib
+
+# The variable for the libraries include directory
+LIBINCDIRS:=$(addprefix -I../lib-,$(LIBS))
+LIBINCDIRS+=$(addsuffix /include, $(LIBINCDIRS))
+
+# The variables for the ld -L flag
+LIBGD32=$(addprefix -L../lib-,$(LIBS))
+LIBGD32:=$(addsuffix /lib_gd32, $(LIBGD32))
+
+# The variable for the ld -l flag
+LDLIBS:=$(addprefix -l,$(LIBS))
+
+# The variables for the dependency check
+LIBDEP=$(addprefix ../lib-,$(LIBS))
+
+DEFINES+=-DHTTPD_CONTENT_SIZE=4096
+DEFINES+=-DTCP_MAX_TCBS_ALLOWED=6
+DEFINES+=-DCONFIG_NETWORK_MEMORY_BLOCKS=10
+DEFINES+=-DDMA_MEMCPY32_DISABLE_IRQ
+
+COPS=-DGD32 -D$(FAMILY_UCA) -D$(LINE_UC) -D$(MCU) -D$(BOARD) -DPHY_TYPE=$(ENET_PHY)
+COPS+=$(strip $(DEFINES) $(MAKE_FLAGS) $(INCLUDES) $(LIBINCDIRS))
+COPS+=$(strip $(ARMOPS) $(CMSISOPS))
+COPS+=-Os -nostartfiles -ffreestanding -nostdlib
+COPS+=-fstack-usage
+COPS+=-ffunction-sections -fdata-sections
+COPS+=-Wall -Werror -Wpedantic -Wextra -Wunused -Wsign-conversion -Wconversion -Wduplicated-cond -Wlogical-op
+COPS+=--specs=nosys.specs
+
+include ../common/make/CppOps.mk
+
+LDOPS=--gc-sections --print-gc-sections --print-memory-usage
+
+PLATFORM_LIBGCC+= -L $(shell dirname `$(CC) $(COPS) -print-libgcc-file-name`)
+
+$(info $$PLATFORM_LIBGCC [${PLATFORM_LIBGCC}])
+
+C_OBJECTS=$(foreach sdir,$(SRCDIR),$(patsubst $(sdir)/%.c,$(BUILD)$(sdir)/%.o,$(wildcard $(sdir)/*.c)))
+CPP_OBJECTS+=$(foreach sdir,$(SRCDIR),$(patsubst $(sdir)/%.cpp,$(BUILD)$(sdir)/%.o,$(wildcard $(sdir)/*.cpp)))
+ASM_OBJECTS=$(foreach sdir,$(SRCDIR),$(patsubst $(sdir)/%.S,$(BUILD)$(sdir)/%.o,$(wildcard $(sdir)/*.S)))
+
+BUILD_DIRS:=$(addprefix $(BUILD),$(SRCDIR))
+
+OBJECTS:=$(strip $(ASM_OBJECTS) $(C_OBJECTS) $(CPP_OBJECTS))
+
+define compile-objects
+$(BUILD)$1/%.o: $1/%.cpp
+	$(CPP) $(COPS) $(CPPOPS) -c $$< -o $$@
+
+$(BUILD)$1/%.o: $1/%.c
+	$(CC) $(COPS) -c $$< -o $$@
+
+$(BUILD)$1/%.o: $1/%.S
+	$(CC) $(COPS) -D__ASSEMBLY__ -c $$< -o $$@
+endef
+
+all : builddirs prerequisites $(TARGET)
+
+.PHONY: clean builddirs
+
+builddirs:
+	mkdir -p $(BUILD_DIRS)
+
+.PHONY:  clean
+
+clean: $(LIBDEP)
+	rm -rf $(BUILD)
+	rm -f $(TARGET)
+	rm -f $(MAP)
+	rm -f $(LIST)
+
+#
+# Libraries
+#
+
+.PHONY: libdep $(LIBDEP)
+
+lisdep: $(LIBDEP)
+
+$(LIBDEP):
+	$(MAKE) -f Makefile.GD32 $(MAKECMDGOALS) 'PROJECT=${PROJECT}' 'FAMILY=${FAMILY}' 'MCU=${MCU}' 'BOARD=${BOARD}' 'ENET_PHY=${ENET_PHY}' 'MAKE_FLAGS=$(DEFINES)' -C $@
+
+#
+# Build bin
+#
+
+$(BUILD)startup_$(LINE).o : $(FIRMWARE_DIR)/startup_$(LINE).S
+	$(AS) $(COPS) -D__ASSEMBLY__ -c $(FIRMWARE_DIR)/startup_$(LINE).S -o $(BUILD)startup_$(LINE).o
+
+$(BUILD)hardfault_handler.o : $(FIRMWARE_DIR)/hardfault_handler.cpp	
+	$(CPP) $(COPS) $(CPPOPS) -c $(FIRMWARE_DIR)/hardfault_handler.cpp -o $(BUILD)hardfault_handler.o
+
+$(BUILD)main.elf: Makefile.GD32 $(LINKER) $(BUILD)startup_$(LINE).o $(BUILD)hardfault_handler.o $(OBJECTS) $(LIBDEP)
+	$(LD) $(BUILD)startup_$(LINE).o $(BUILD)hardfault_handler.o $(OBJECTS) -Map $(MAP) -T $(LINKER) $(LDOPS) -o $(BUILD)main.elf $(LIBGD32) $(LDLIBS) $(PLATFORM_LIBGCC) -lgcc
+	$(PREFIX)objdump -D $(BUILD)main.elf | $(PREFIX)c++filt > $(LIST)
+	$(PREFIX)size -A -x $(BUILD)main.elf
+
+$(TARGET) : $(BUILD)main.elf
+	$(PREFIX)objcopy $(BUILD)main.elf -O binary $(TARGET) --remove-section=.tcmsram* --remove-section=.ram* --remove-section=.sram1* --remove-section=.sram2* --remove-section=.ramadd* --remove-section=.bkpsram*
+
+$(foreach bdir,$(SRCDIR),$(eval $(call compile-objects,$(bdir))))
---
+++ firmware-template-gd32/lib/Rules.mk
@@ -0,0 +1,97 @@
+$(info "lib/Rules.mk")
+$(info $$MAKE_FLAGS [${MAKE_FLAGS}])
+
+PREFIX ?= arm-none-eabi-
+
+CC	= $(PREFIX)gcc
+CPP	= $(PREFIX)g++
+AS	= $(CC)
+LD	= $(PREFIX)ld
+AR	= $(PREFIX)ar
+
+BOARD?=BOARD_GD32F470VG
+ENET_PHY?=RTL8201F
+
+$(info $$BOARD [${BOARD}])
+$(info $$ENET_PHY [${ENET_PHY}])
+
+SRCDIR=src src/gd32 $(EXTRA_SRCDIR)
+
+DEFINES:=$(addprefix -D,$(DEFINES))
+
+include ../common/make/gd32/Board.mk
+include ../common/make/gd32/Mcu.mk
+include ../common/make/DmxNodeNodeType.mk
+include ../common/make/DmxNodeOutputType.mk
+include ../common/make/gd32/Includes.mk
+include ../common/make/Artnet.mk
+include ../common/make/gd32/Validate.mk
+
+INCLUDES+=-I../lib-configstore/include -I../lib-device/include -I../lib-display/include -I../lib-flash/include -I../lib-flashcode/include -I../lib-hal/include -I../lib-network/include
+
+COPS=-DGD32 -D$(FAMILY_UCA) -D$(LINE_UC) -D$(MCU) -D$(BOARD) -DPHY_TYPE=$(ENET_PHY)
+COPS+=$(strip $(DEFINES) $(MAKE_FLAGS) $(VALIDATE_FLAGS) $(INCLUDES))
+COPS+=$(strip $(ARMOPS) $(CMSISOPS))
+COPS+=-Os -nostartfiles -ffreestanding -nostdlib
+COPS+=-fstack-usage
+COPS+=-ffunction-sections -fdata-sections
+COPS+=-Wall -Werror -Wpedantic -Wextra -Wunused -Wsign-conversion -Wduplicated-cond -Wlogical-op
+ifndef FREE_RTOS_PORTABLE
+COPS+=-Wconversion
+endif
+
+include ../common/make/CppOps.mk
+
+BUILD=build_gd32/
+BUILD_DIRS:=$(addprefix build_gd32/,$(SRCDIR))
+
+include ../common/make/lib/Objects.mk
+
+CURR_DIR:=$(notdir $(patsubst %/,%,$(CURDIR)))
+LIB_NAME:=$(patsubst lib-%,%,$(CURR_DIR))
+TARGET=lib_gd32/lib$(LIB_NAME).a
+
+$(info $$BUILD_DIRS [${BUILD_DIRS}])
+$(info $$EXTRA_C_BUILD_DIRS [${EXTRA_C_BUILD_DIRS}])
+$(info $$EXTRA_CPP_BUILD_DIRS [${EXTRA_CPP_BUILD_DIRS}])
+$(info $$DEFINES [${DEFINES}])
+$(info $$MAKE_FLAGS [${MAKE_FLAGS}])
+$(info $$OBJECTS [${OBJECTS}])
+$(info $$TARGET [${TARGET}])
+
+define compile-objects
+$(info $1)
+$(BUILD)$1/%.o: $1/%.c
+	$(CC) -MD -MP $(COPS) -c $$< -o $$@
+
+$(BUILD)$1/%.o: $1/%.cpp
+	$(CPP) -MD -MP $(COPS) $(CPPOPS) -c $$< -o $$@
+
+-include $(BUILD)$1/*.d
+
+$(BUILD)$1/%.o: $1/%.S
+	$(CC) $(COPS) -D__ASSEMBLY__ -c $$< -o $$@
+endef
+
+all : builddirs $(TARGET)
+
+.PHONY: clean builddirs
+
+builddirs:
+	mkdir -p $(BUILD_DIRS) 
+	if [[ -n "${EXTRA_C_BUILD_DIRS}" ]]; then mkdir -p $(EXTRA_C_BUILD_DIRS); fi
+	if [[ -n "${EXTRA_CPP_BUILD_DIRS}" ]]; then mkdir -p $(EXTRA_CPP_BUILD_DIRS); fi
+	mkdir -p lib_gd32
+
+clean:
+	rm -rf build_gd32
+	rm -rf lib_gd32
+	
+$(BUILD)%.o: %.c
+	$(CC) $(COPS) -c $< -o $@
+
+$(TARGET): Makefile.GD32 $(OBJECTS)
+	$(AR) -r $(TARGET) $(OBJECTS)
+	$(PREFIX)objdump -d $(TARGET) | $(PREFIX)c++filt > lib_gd32/lib.list
+	
+$(foreach bdir,$(SRCDIR),$(eval $(call compile-objects,$(bdir))))
